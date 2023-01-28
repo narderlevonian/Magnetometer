fbb11e0722d2a1d853c54e0a32a8f4ba79d651c9
Nar der Levonian <narderlevonian@icloud.com>
2023-01-28 14:29:51 +0300

    Initial Commit

---
+++ Magnetometer/MagnetometerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MagnetometerApp.swift
+//  Magnetometer
+//
+//  Created by Nar der Levonian on 28.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct MagnetometerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
